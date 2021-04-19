f48dfe9c39c82cf542e1dd707671cb515722a33a
Anastasiia Shtanko <anastasiia.shtanko@globallogic.com>
2021-04-20 02:38:48 +0300

    Learning project

---
+++ Parent.py
@@ -0,0 +1,15 @@
+import datetime
+
+
+class Parent(datetime):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def currDate(self):
+        temp = datetime.datetime.now()
+        print("Current date: ")
+        print(temp.year, ".", temp.month, ".", temp.day)
+        return temp
+
+
